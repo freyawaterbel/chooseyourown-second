7bb4ce3cda8c7d556c8b52fdcce4a0ce07949ae9
freyawaterbel <freyawaterbel@gmail.com>
2023-12-07 18:21:43 -0500

    Initial build

---
+++ chooseyourown/src/main/java/com/fwaterbel/chooseyourown/ChooseyourownApplication.java
@@ -0,0 +1,13 @@
+package com.fwaterbel.chooseyourown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseyourownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseyourownApplication.class, args);
+	}
+
+}
---
+++ chooseyourown/src/test/java/com/fwaterbel/chooseyourown/ChooseyourownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwaterbel.chooseyourown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooseyourownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
